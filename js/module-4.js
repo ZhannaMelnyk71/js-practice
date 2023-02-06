3ff9df48cecc4b25bb9c0298b5b111d570046b1b
ZhannaMelnyk71 <11mazourenko@gmail.com>
2023-02-07 00:37:37 +0200

    mod-4

---
+++ js/module-4.js
@@ -0,0 +1,69 @@
+// Завдання 1
+
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// // Change code below this line
+
+// const result = makePizza("Your pizza is being prepared, please wait.");
+// const pointer = makePizza;
+
+// Завдання 2
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+// makeMessage("Royal Grand", makePizza);
+// makeMessage("Ultracheese", deliverPizza);
+
+// Завдання 3
+
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}.`);
+// });
+// // Change code below this line
+
+// makePizza("Ultracheese", function eatPizza(pizzaName) {
+//   console.log(`Eating pizza ${pizzaName}.`);
+// });
+
+// Завдання 4
+
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//   order(pizzaName, onSuccess, onError) {
+//     const error = `There is no pizza with a name ${pizzaName} in the assortment.`;
+
+//     if (this.pizzas.includes(pizzaName)) {
+//       return makePizza(pizzaName);
+//     }
+//     return onOrderError(error);
+//   },
+// };
+// // Change code above this line
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// pizzaPalace.order("Smoked", makePizza, onOrderError);
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
