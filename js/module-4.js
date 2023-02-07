f0f4bda74f1d75bf020b341580e736bd185d5060
ZhannaMelnyk71 <11mazourenko@gmail.com>
2023-02-07 02:34:34 +0200

    mod4-test-1-20

--- js/module-4.js
+++ js/module-4.js
@@ -67,3 +67,249 @@
 // pizzaPalace.order("Four meats", makePizza, onOrderError);
 // pizzaPalace.order("Big Mike", makePizza, onOrderError);
 // pizzaPalace.order("Vienna", makePizza, onOrderError);
+
+// Завдання 5
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Change code below this line
+
+//   // for (let i = 0; i < orderedItems.length; i += 1) {
+//   //   totalPrice += orderedItems[i];
+//   // }
+
+//   orderedItems.forEach(function (number, index) {
+//     totalPrice += orderedItems[index];
+//   });
+
+//   // Change code above this line
+//   return totalPrice;
+// }
+// calculateTotalPrice([12, 85, 37, 4]);
+
+// Завдання 6
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   // Change code below this line
+
+//   // for (let i = 0; i < numbers.length; i += 1) {
+//   //   if (numbers[i] > value) {
+//   //     filteredNumbers.push(numbers[i]);
+//   //   }
+//   // }
+//   numbers.forEach(function (number, index) {
+//     if (number > value) {
+//       filteredNumbers.push(numbers[index]);
+//     }
+//   });
+
+//   // Change code above this line
+//   return filteredNumbers;
+// }
+// filterArray([1, 2, 3, 4, 5], 3);
+
+// Завдання 7
+
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   // Change code below this line
+
+//   // for (let i = 0; i < firstArray.length; i += 1) {
+//   //   if (secondArray.includes(firstArray[i])) {
+//   //     commonElements.push(firstArray[i]);
+//   //   }
+//   // }
+//   firstArray.forEach(function (number, index) {
+//     if (secondArray.includes(firstArray[index])) {
+//       commonElements.push(firstArray[index]);
+//     }
+//   });
+
+//   return commonElements;
+//   // Change code above this line
+// }
+// getCommonElements([1, 2, 3], [2, 4]);
+
+// Завдання 8
+
+// const calculateTotalPrice = (quantity, pricePerItem) => {
+//   return quantity * pricePerItem;
+// };
+
+// Завдання 9
+
+// Change code below this line
+// const calculateTotalPrice = (quantity, pricePerItem) =>  quantity * pricePerItem;
+// Change code above this line
+
+// const add = (a, b, c) => {
+//   console.log(a, b, c);
+//   return a + b + c;
+// };
+
+// const add = (a, b, c) => a + b + c;
+
+// Завдання 10
+
+// const calculateTotalPrice = (orderedItems) => {
+//   let totalPrice = 0;
+//   orderedItems.forEach((item) => {
+//     totalPrice += item;
+//   });
+//   return totalPrice;
+// };
+
+// // Change code below this line
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+
+//   numbers.forEach(function (number) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   // Change code above this line
+//   return filteredNumbers;
+// }
+
+// Завдання 11
+
+// const filterArray = (numbers,value) => {
+//   const filteredNumbers = [];
+
+//   numbers.forEach(number => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+//   return filteredNumbers;
+// }
+
+// Завдання 12
+
+// const getCommonElements = (firstArray, secondArray) => {
+//   const commonElements = [];
+
+//   firstArray.forEach((element) => {
+//     if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+//   });
+//   return commonElements;
+// };
+
+// Завдання 12
+
+// function changeEven(numbers, value) {
+//   const newArray = [];
+
+//   numbers.forEach((number) => {
+//     if (number % 2 === 0) {
+//       newArray.push(number + value);
+//     } else {
+//       newArray.push(number);
+//     }
+//   });
+
+//   return newArray;
+//   //   // Change code below this line
+//   //   for (let i = 0; i < numbers.length; i += 1) {
+//   //     if (numbers[i] % 2 === 0) {
+//   //       numbers[i] = numbers[i] + value;
+//   //     }
+//   //   }
+//   //   // Change code above this line
+// }
+// changeEven([1, 2, 3, 4, 5], 10);
+
+// Завдання 12
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// // Change code below this line
+// const planetsLengths = planets.map(planet => planet.length);;
+
+// Завдання 13
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const titles = books.map((book) => book.title);
+
+// Завдання 16
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism"],
+//   },
+// ];
+// // Change code below this line
+
+// const genres = books.flatMap((book) => book.genres);
+
+// Завдання 17
+
+// Change code below this line
+// const getUserNames = users => {
+//    return users.map(user => user.name)
+
+//   };
+
+// Завдання 19
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter((number) => number % 2 === 0);
+// const oddNumbers = numbers.filter((number) => number % 2 > 0);
+
+// Завдання 19
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction", "mysticism"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism", "adventure"],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap((book) => book.genres);
+// const uniqueGenres = allGenres.filter(
+//   (genre, index, array) => array.indexOf(genre) === index
+// );
